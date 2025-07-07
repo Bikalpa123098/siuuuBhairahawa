49a25146b05867775a93e9a330038cf77aa3e8ad
bikalpa123098 <koiralabikalpa665@gmail.com>
2025-07-07 18:30:25 +0545

    only comment 6:30 7-7-2025

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import Navbar from '../Components/PageComponents/Navbar'
+import React from "react";
+import Navbar from "../Components/PageComponents/Navbar";
 
 function Home() {
   return (
-    <div className='bg-[#83B5D1] p-10 h-screen w-full '>
-        <Navbar/>
+    <div className="bg-[#83B5D1] p-10 h-screen w-full ">
+      <Navbar />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
+
+//https://coolors.co/673c4f-7f557d-726e97-7698b3-83b5d1
+//https://coolors.co/673c4f-7f557d-726e97-7698b3-83b5d1
